2dd4fd5b2125d34a507c7e4622d382bd8d95859e
vitalreddy <vitalreddy.c@gmail.com>
2023-02-04 07:18:35 -0800

    pytestforcompare

---
+++ testcompare.py
@@ -0,0 +1,14 @@
+
+
+#testfile with compare operator
+
+def test_greater():
+    num=100
+    assert num > 50
+def test_less():
+    num=100
+    assert num<150
+def test_gretaer_equal():
+    num=200
+    assert num >= 200
+
